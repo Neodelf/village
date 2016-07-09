54d90eb2a7cc1fe714768c77b41b64e5e2b202e5
Molchanov Andrey <neodelf@gmail.com>
2016-07-09 22:00:25 +0300

    change admin/stead.rb

--- app/admin/stead.rb
+++ app/admin/stead.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Stead do
   permit_params :cost_square_meter, :serial_number, :total_area, :description
+  actions :edit, :update, :index, :show
+  config.per_page = 20
+  config.sort_order = 'serial_number'
+
+  index do
+    column :serial_number
+    column :total_area
+    column :cost_square_meter
+    column :description
+    actions
+  end
 end
