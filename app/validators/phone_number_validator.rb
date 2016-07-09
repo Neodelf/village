b5f2a4fdb08d1e96ee7d2b49c5c9e95dcb2a6b26
Molchanov Andrey <neodelf@gmail.com>
2016-07-09 19:10:47 +0300

    fix phone_number validator

--- app/validators/phone_number_validator.rb
+++ app/validators/phone_number_validator.rb
@@ -1,4 +1,4 @@
-class PhoneValidator < ActiveModel::EachValidator
+class PhoneNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}\z/
       record.errors[attribute] << (options[:message] || I18n.t('activerecord.errors.wrong_format'))
