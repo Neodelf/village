979da54699a56781a5afdcf3d5073e26debaaf7f
Molchanov Andrey <neodelf@gmail.com>
2016-07-09 21:30:08 +0300

    add default settings to migrate

--- db/migrate/20160709141055_create_settings.rb
+++ db/migrate/20160709141055_create_settings.rb
@@ -3,5 +3,6 @@ class CreateSettings < ActiveRecord::Migration
     create_table :settings do |t|
       t.integer :default_cost_square_meter
     end
+    Setting.create!(default_cost_square_meter: 450)
   end
 end
