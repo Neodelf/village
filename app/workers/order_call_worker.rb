ddf3e01e8f60529444cb1b2958aabb5c5e431ce8
Molchanov Andrey <neodelf@gmail.com>
2016-07-14 00:50:49 +0300

    change queue for worker

--- app/workers/order_call_worker.rb
+++ app/workers/order_call_worker.rb
@@ -1,6 +1,6 @@
 class OrderCallWorker
   include Sidekiq::Worker
-  sidekiq_options retry: true, backtrace: true
+  sidekiq_options queue: :village_prod, retry: true, backtrace: true
 
   def perform(id)
     CommonEmail.new_order_call(id).deliver
